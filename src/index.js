0f7b0cc23ad76f6ad204a37961175aaecb94fe3c
wbdvlpmnt <jai.gokhale5219@gmail.com>
2024-02-12 09:32:04 -0600

    wrap app in providerc

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./Components/App";
 import "./index.css";
+import AppProvider from "./Components/AppProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppProvider>
+      <App />
+    </AppProvider>
   </React.StrictMode>
 );
