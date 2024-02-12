0c1ffe876bdc93c9dd89986f2512f357d0c93a19
wbdvlpmnt <jai.gokhale5219@gmail.com>
2024-02-12 09:31:32 -0600

    setting up the provider

--- src/Components/App.js
+++ src/Components/App.js
@@ -11,81 +11,10 @@ import FinishScreen from "./FinishScreen";
 import Footer from "./Footer";
 import Timer from "./Timer";
 
-const initialState = {
-  questions: [],
-  status: "loading",
-  index: 0,
-  answer: null,
-  points: 0,
-  highScore: 0,
-  secondsRemaining: null,
-};
 
-const SECS_PER_QUESTION = 30;
-
-function reducer(state, action) {
-  switch (action.type) {
-    case "dataRecieved":
-      return { ...state, questions: action.payload, status: "ready" };
-    case "dataFailed":
-      return { ...state, status: "error" };
-    case "start":
-      return {
-        ...state,
-        status: "active",
-        secondsRemaining: state.questions.length * SECS_PER_QUESTION,
-      };
-    case "newAnswer":
-      const question = state.questions.at(state.index);
-      return {
-        ...state,
-        answer: action.payload,
-        points:
-          action.payload === question.correctOption
-            ? state.points + question.points
-            : state.points,
-      };
-    case "nextQuestion":
-      return { ...state, index: state.index + 1, answer: null };
-    case "finish":
-      return {
-        ...state,
-        status: "finished",
-        highScore:
-          state.points > state.highScore ? state.points : state.highScore,
-      };
-    case "restart":
-      return { ...initialState, questions: state.questions, status: "ready" };
-    case "tick":
-      return {
-        ...state,
-        secondsRemaining: state.secondsRemaining - 1,
-        status: state.secondsRemaining === 0 ? "finished" : state.status,
-      };
-    default:
-      throw new Error("Action is unknown");
-  }
-}
 
 function App() {
-  const [
-    { questions, status, index, answer, points, highScore, secondsRemaining },
-    dispatch,
-  ] = useReducer(reducer, initialState);
 
-  const numQuestions = questions.length;
-
-  const maxPossiblePoints = questions.reduce(
-    (prev, cur) => prev + cur.points,
-    0
-  );
-
-  useEffect(() => {
-    fetch("http://localhost:8000/questions")
-      .then((res) => res.json())
-      .then((data) => dispatch({ type: "dataRecieved", payload: data }))
-      .catch((error) => dispatch({ type: "dataFailed" }));
-  }, []);
   return (
     <div className="App">
       <Header />
--- src/Components/AppProvider.js
+++ src/Components/AppProvider.js
@@ -1,14 +1,109 @@
-import React, { createContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 export const AppContext = createContext();
 
-export default function AppProvider({children}){
+const initialState = {
+  questions: [],
+  status: "loading",
+  index: 0,
+  answer: null,
+  points: 0,
+  highScore: 0,
+  secondsRemaining: null,
+};
 
-    return(
-        <AppContext.Provider
-            value={}
-        >
-        {children}
-        </AppContext.Provider>
-    )
+const SECS_PER_QUESTION = 30;
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecieved":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    case "start":
+      return {
+        ...state,
+        status: "active",
+        secondsRemaining: state.questions.length * SECS_PER_QUESTION,
+      };
+    case "newAnswer":
+      const question = state.questions.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+    case "nextQuestion":
+      return { ...state, index: state.index + 1, answer: null };
+    case "finish":
+      return {
+        ...state,
+        status: "finished",
+        highScore:
+          state.points > state.highScore ? state.points : state.highScore,
+      };
+    case "restart":
+      return { ...initialState, questions: state.questions, status: "ready" };
+    case "tick":
+      return {
+        ...state,
+        secondsRemaining: state.secondsRemaining - 1,
+        status: state.secondsRemaining === 0 ? "finished" : state.status,
+      };
+    default:
+      throw new Error("Action is unknown");
+  }
 }
+
+export default function AppProvider({ children }) {
+  const [
+    { questions, status, index, answer, points, highScore, secondsRemaining },
+    dispatch,
+  ] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
+
+  const maxPossiblePoints = questions.reduce(
+    (prev, cur) => prev + cur.points,
+    0
+  );
+
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataRecieved", payload: data }))
+      .catch((error) => dispatch({ type: "dataFailed" }));
+  }, []);
+
+  return (
+    <AppContext.Provider
+      value={
+        (questions,
+        status,
+        index,
+        answer,
+        points,
+        highScore,
+        secondsRemaining,
+        numQuestions,
+        maxPossiblePoints,
+        dispatch)
+      }
+    >
+      {children}
+    </AppContext.Provider>
+  );
+}
+
+function useQuiz() {
+  const context = useContext(AppContext);
+  if (context === undefined) {
+    throw new Error("Context is used outside of the provider.");
+  }
+  return context;
+}
+
+export { AppProvider, useQuiz };
